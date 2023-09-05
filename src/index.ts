804f4c664e90713d83cdb4d9ddd76fb60ae6a957
NamesMT <dangquoctrung123@gmail.com>
2023-09-05 08:53:25 +0000

    example commit

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,9 @@
 export function test() {
   return "works!";
 }
+
+import type { Context } from 'aws-lambda'
+
+export const halo = (e?: any, c?: Context) => { return {} as any}
+
+console.log({hi: halo})
